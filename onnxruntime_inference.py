f4042e452f274a6d945c37848e6c85267e690c6c
laitathei <56575156+laitathei@users.noreply.github.com>
2023-08-08 10:54:45 +0800

    Update onnxruntime_inference.py for no segment result

--- onnxruntime_inference.py
+++ onnxruntime_inference.py
@@ -15,7 +15,6 @@ video_path = "test.mp4"
 video_inference = False
 CLASSES = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis','snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
 
-
 sess = onnxruntime.InferenceSession(model_path) # model_height_width.onnx
 images = sess.get_inputs()[0].name
 output0 = sess.get_outputs()[0].name
@@ -33,7 +32,7 @@ if video_inference == True:
         results, scores = postprocess(outputs, image_4c, image_3c, conf_thres, iou_thres, classes=len(CLASSES)) ##[box,mask,shape]
         results = results[0]              ## batch=1,取第一个数据即可
         boxes, masks, shape = results
-        if masks.ndim == 3:
+        if isinstance(masks, np.ndarray):
             mask_img, vis_img = vis_result(image_3c,  results, colorlist, CLASSES, result_path , scores)
             cv2.imshow("mask_img", mask_img)
             cv2.imshow("vis_img", vis_img)
@@ -48,7 +47,7 @@ else:
     results, scores = postprocess(outputs, image_4c, image_3c, conf_thres, iou_thres, classes=len(CLASSES)) ##[box,mask,shape]
     results = results[0]              ## batch=1,取第一个数据即可
     boxes, masks, shape = results
-    if masks.ndim == 3:
+    if isinstance(masks, np.ndarray):
         mask_img, vis_img = vis_result(image_3c,  results, colorlist, CLASSES, result_path, scores)
         print('--> Save inference result')
     else:
