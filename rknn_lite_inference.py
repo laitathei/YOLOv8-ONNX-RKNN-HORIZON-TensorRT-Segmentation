700cbfb1784e97bad450e039c7dee5593ae312c1
laitathei <56575156+laitathei@users.noreply.github.com>
2023-08-08 23:16:37 +0800

    Update rknn_lite_inference.py

--- rknn_lite_inference.py
+++ rknn_lite_inference.py
@@ -13,7 +13,7 @@ result_path = "./result"
 image_path = "./dataset/bus.jpg"
 video_path = "test.mp4"
 video_inference = False
-RKNN_MODEL = f'./{model_path}/yolov8n-seg-{input_height}-{input_width}.rknn'
+RKNN_MODEL = f'{model_path}/yolov8n-seg-{input_height}-{input_width}.rknn'
 CLASSES = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis','snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
 isExist = os.path.exists(result_path)
 if not isExist:
