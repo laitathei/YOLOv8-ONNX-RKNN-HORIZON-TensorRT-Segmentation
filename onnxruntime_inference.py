6a477b5cf6481f2a45c0ff7d7a4e60e62e944353
laitathei <56575156+laitathei@users.noreply.github.com>
2023-08-08 11:02:06 +0800

    Update onnxruntime_inference.py

--- onnxruntime_inference.py
+++ onnxruntime_inference.py
@@ -15,7 +15,7 @@ video_path = "test.mp4"
 video_inference = False
 CLASSES = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis','snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
 
-sess = onnxruntime.InferenceSession(model_path) # model_height_width.onnx
+sess = onnxruntime.InferenceSession(model_path)
 images = sess.get_inputs()[0].name
 output0 = sess.get_outputs()[0].name
 output1 = sess.get_outputs()[1].name
@@ -31,10 +31,10 @@ if video_inference == True:
             break
         image_4c, image_3c = preprocess(image_3c, input_height, input_width)
         print('--> Running model for video inference')
-        outputs = sess.run([output0, output1],{images: image_4c.astype(np.float32)}) # (1, 2, 352, 640)
-        colorlist = gen_color(len(CLASSES))  ## 获取着色时的颜色信息
+        outputs = sess.run([output0, output1],{images: image_4c.astype(np.float32)}) # (1, 3, input height, input width)
+        colorlist = gen_color(len(CLASSES))
         results, scores = postprocess(outputs, image_4c, image_3c, conf_thres, iou_thres, classes=len(CLASSES)) ##[box,mask,shape]
-        results = results[0]              ## batch=1,取第一个数据即可
+        results = results[0]              ## batch=1
         boxes, masks, shape = results
         if isinstance(masks, np.ndarray):
             mask_img, vis_img = vis_result(image_3c,  results, colorlist, CLASSES, result_path , scores)
@@ -46,10 +46,10 @@ if video_inference == True:
 else:
     image_3c = cv2.imread(image_path)
     image_4c, image_3c = preprocess(image_3c, input_height, input_width)
-    outputs = sess.run([output0, output1],{images: image_4c.astype(np.float32)}) # (1, 2, 352, 640)
-    colorlist = gen_color(len(CLASSES))  ## 获取着色时的颜色信息
+    outputs = sess.run([output0, output1],{images: image_4c.astype(np.float32)}) # (1, 3, input height, input width)
+    colorlist = gen_color(len(CLASSES)) 
     results, scores = postprocess(outputs, image_4c, image_3c, conf_thres, iou_thres, classes=len(CLASSES)) ##[box,mask,shape]
-    results = results[0]              ## batch=1,取第一个数据即可
+    results = results[0]              ## batch=1
     boxes, masks, shape = results
     if isinstance(masks, np.ndarray):
         mask_img, vis_img = vis_result(image_3c,  results, colorlist, CLASSES, result_path, scores)
