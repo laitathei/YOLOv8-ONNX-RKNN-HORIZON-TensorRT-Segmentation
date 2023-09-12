578ee45f2018c01305dab4764d45b63f990952e9
laitathei <56575156+laitathei@users.noreply.github.com>
2023-09-12 17:46:18 +0800

    Add files via upload

---
+++ onnx2trt.py
@@ -0,0 +1,73 @@
+import os, time, json
+from datetime import datetime
+import tensorrt as trt
+
+task = "segment"
+batch_size = 1
+input_width = 640
+input_height = 480
+precision = "FP16" # FP16, INT8
+workspace = 1 # number of GB
+model_path = "./model"
+model_name = 'yolov8n-seg'
+ONNX_MODEL = f'{model_path}/{model_name}-{input_height}-{input_width}.onnx'
+TensorRT_MODEL = f'{model_path}/{model_name}-{input_height}-{input_width}.engine'
+CLASSES = {0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 4: 'airplane', 5: 'bus', 6: 'train', 7: 'truck', 8: 'boat', 9: 'traffic light', 10: 'fire hydrant', 11: 'stop sign', 12: 'parking meter', 13: 'bench', 14: 'bird', 15: 'cat', 16: 'dog', 17: 'horse', 18: 'sheep', 19: 'cow', 20: 'elephant', 21: 'bear', 22: 'zebra', 23: 'giraffe', 24: 'backpack', 25: 'umbrella', 26: 'handbag', 27: 'tie', 28: 'suitcase', 29: 'frisbee', 30: 'skis', 31: 'snowboard', 32: 'sports ball', 33: 'kite', 34: 'baseball bat', 35: 'baseball glove', 36: 'skateboard', 37: 'surfboard', 38: 'tennis racket', 39: 'bottle', 40: 'wine glass', 41: 'cup', 42: 'fork', 43: 'knife', 44: 'spoon', 45: 'bowl', 46: 'banana', 47: 'apple', 48: 'sandwich', 49: 'orange', 50: 'broccoli', 51: 'carrot', 52: 'hot dog', 53: 'pizza', 54: 'donut', 55: 'cake', 56: 'chair', 57: 'couch', 58: 'potted plant', 59: 'bed', 60: 'dining table', 61: 'toilet', 62: 'tv', 63: 'laptop', 64: 'mouse', 65: 'remote', 66: 'keyboard', 67: 'cell phone', 68: 'microwave', 69: 'oven', 70: 'toaster', 71: 'sink', 72: 'refrigerator', 73: 'book', 74: 'clock', 75: 'vase', 76: 'scissors', 77: 'teddy bear', 78: 'hair drier', 79: 'toothbrush'}
+
+if __name__ == '__main__':
+    # Build TensorRT engine from Onnx file
+    logger = trt.Logger(trt.Logger.INFO)
+    builder = trt.Builder(logger)
+    config = builder.create_builder_config()
+    config.max_workspace_size = workspace * 1 << 30 # in terms of GB
+
+    #config.set_memory_pool_limit(trt.MemoryPoolType.WORKSPACE, workspace * (2 ** 30)) # in terms of GB
+    flag = 1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
+    network = builder.create_network(flag)
+    parser = trt.OnnxParser(network, logger)
+
+    # Parse model file
+    if not os.path.exists(ONNX_MODEL):
+        print('ONNX file {} not found, please run pytorch2onnx.py first to generate it.'.format(ONNX_MODEL))
+        exit(0)
+    if not parser.parse_from_file(ONNX_MODEL):
+        raise RuntimeError(f'failed to load ONNX file: {ONNX_MODEL}')
+
+    inputs = [network.get_input(i) for i in range(network.num_inputs)]
+    outputs = [network.get_output(i) for i in range(network.num_outputs)]
+
+    print("Network Description")
+    for input in inputs:
+        print("Input '{}' with shape {} and dtype {}".format(input.name, input.shape, input.dtype))
+    for output in outputs:
+        print("Output '{}' with shape {} and dtype {}".format(output.name, output.shape, output.dtype))
+
+    if builder.platform_has_fast_fp16 and precision == "FP16": # build with fp16
+        config.set_flag(trt.BuilderFlag.FP16)
+    elif builder.platform_has_fast_int8 and precision == "INT8": # build with int8
+        config.set_flag(trt.BuilderFlag.INT8)
+
+    start = time.time()
+    metadata = {
+        'description': "Ultralytics YOLOv8n-seg model (untrained)",
+        'author': 'Ultralytics',
+        'license': 'MIT License',
+        'date': datetime.now().isoformat(),
+        'version': "8.0.147",
+        'stride': 32,
+        'task': task,
+        'batch': batch_size,
+        'imgsz': [input_height, input_width],
+        'names': CLASSES}  # model metadata
+    print(metadata)
+    # Write file
+    with builder.build_engine(network, config) as engine, open(TensorRT_MODEL, 'wb') as t:
+        # Metadata
+        meta = json.dumps(metadata)
+        t.write(len(meta).to_bytes(4, byteorder='little', signed=True))
+        t.write(meta.encode())
+        # Model
+        t.write(engine.serialize())
+    print("used time: ", time.time()-start)
+    print("Saved TensorRT Engine")
+    
---
+++ tensorrt_inference.py
@@ -0,0 +1,97 @@
+import cv2, json
+import tensorrt as trt
+import pycuda.driver as cuda
+import pycuda.autoinit
+from utils import *
+import torch
+from collections import OrderedDict, namedtuple
+
+conf_thres = 0.25
+iou_thres = 0.45
+input_width = 640
+input_height = 480
+result_path = "./result"
+image_path = "./dataset/bus.jpg"
+model_name = 'yolov8n-seg'
+model_path = "./model"
+ONNX_MODEL = f'{model_path}/{model_name}-{input_height}-{input_width}.onnx'
+TensorRT_MODEL = f'{model_path}/{model_name}-{input_height}-{input_width}.engine'
+video_path = "test.mp4"
+video_inference = True
+CLASSES = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis','snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+
+def main():
+    Binding = namedtuple('Binding', ('name', 'dtype', 'shape', 'data', 'ptr'))
+    logger = trt.Logger(trt.Logger.INFO)
+    device = torch.device('cuda:0')
+    # Read file
+    with open(TensorRT_MODEL, 'rb') as f, trt.Runtime(logger) as runtime:
+        meta_len = int.from_bytes(f.read(4), byteorder='little')  # read metadata length
+        metadata = json.loads(f.read(meta_len).decode('utf-8'))  # read metadata
+        model = runtime.deserialize_cuda_engine(f.read())  # read engine
+    context = model.create_execution_context()
+    bindings = OrderedDict()
+    input_names = []
+    output_names = []
+    for i in range(model.num_bindings):
+        name = model.get_binding_name(i) 
+        dtype = trt.nptype(model.get_binding_dtype(i))
+        if model.binding_is_input(i):
+            input_names.append(name)
+        else:  # output
+            output_names.append(name)
+        shape = tuple(context.get_binding_shape(i))
+        im = torch.from_numpy(np.empty(shape, dtype=dtype)).to(device)
+        bindings[name] = Binding(name, dtype, shape, im, int(im.data_ptr()))
+    binding_addrs = OrderedDict((n, d.ptr) for n, d in bindings.items())
+
+    if video_inference == True:    
+        cap = cv2.VideoCapture(video_path)
+        while(True):
+            ret, image_3c = cap.read()
+            if not ret:
+                break
+            image_4c, image_3c = preprocess(image_3c, input_height, input_width)
+            image_4c = image_4c.astype(np.float32)
+            image_4c = torch.from_numpy(image_4c).to(device)
+            if image_4c.dtype != torch.float16:
+                image_4c = image_4c.half()  # to FP16
+            binding_addrs['images'] = int(image_4c.data_ptr())
+            context.execute_v2(list(binding_addrs.values()))
+            outputs = [bindings[x].data.cpu().numpy() for x in sorted(output_names)] # put the result from gpu to cpu and convert to numpy
+            colorlist = gen_color(len(CLASSES))
+            results = postprocess(outputs, image_4c, image_3c, conf_thres, iou_thres, classes=len(CLASSES)) ##[box,mask,shape]
+            results = results[0]              ## batch=1
+            boxes, masks, shape = results
+            if isinstance(masks, np.ndarray):
+                mask_img, vis_img = vis_result(image_3c,  results, colorlist, CLASSES, result_path)
+                cv2.imshow("mask_img", mask_img)
+                cv2.imshow("vis_img", vis_img)
+            else:
+                print("No segmentation result")
+            cv2.waitKey(10)
+    else:
+        image_3c = cv2.imread(image_path)
+        image_4c, image_3c = preprocess(image_3c, input_height, input_width)
+        image_4c = image_4c.astype(np.float32)
+        image_4c = torch.from_numpy(image_4c).to(device)
+        if image_4c.dtype != torch.float16:
+            image_4c = image_4c.half()  # to FP16
+        binding_addrs['images'] = int(image_4c.data_ptr())
+        context.execute_v2(list(binding_addrs.values()))
+        outputs = [bindings[x].data.cpu().numpy() for x in sorted(output_names)] # put the result from gpu to cpu and convert to numpy
+        colorlist = gen_color(len(CLASSES)) 
+        results = postprocess(outputs, image_4c, image_3c, conf_thres, iou_thres, classes=len(CLASSES)) ##[box,mask,shape]
+        results = results[0]              ## batch=1
+        boxes, masks, shape = results
+        if isinstance(masks, np.ndarray):
+            mask_img, vis_img = vis_result(image_3c,  results, colorlist, CLASSES, result_path)
+            print('--> Save inference result')
+        else:
+            print("No segmentation result")
+    print("TensorRT inference finish")
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
