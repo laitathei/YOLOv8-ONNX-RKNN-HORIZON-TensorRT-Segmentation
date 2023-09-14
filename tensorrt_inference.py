1eb86df69e01ea3e5868e7b8d9faa76a3f6a776a
laitathei <56575156+laitathei@users.noreply.github.com>
2023-09-14 17:39:43 +0800

    Update tensorrt_inference.py

--- tensorrt_inference.py
+++ tensorrt_inference.py
@@ -17,7 +17,7 @@ model_path = "./model"
 ONNX_MODEL = f'{model_path}/{model_name}-{input_height}-{input_width}.onnx'
 TensorRT_MODEL = f'{model_path}/{model_name}-{input_height}-{input_width}.engine'
 video_path = "test.mp4"
-video_inference = True
+video_inference = False
 CLASSES = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis','snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
 
 def main():
